2df8334f4d3efabcdc69a6a83f11f28d6aaa8009
Karolina Pytlos <karolina_pytlos@epam.com>
2024-12-09 14:52:49 +0100

    Updated azure functions project

---
+++ src/AzureFunctions/Responses/DeliveryOrderProcessorResponse.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+
+namespace AzureFunctions.Responses;
+public class DeliveryOrderProcessorResponse
+{
+    [CosmosDBOutput("%CosmosDb%", "%CosmosContainerOut%", Connection = "CosmosDbConnectionSetting", CreateIfNotExists = true)]
+    public string Order { get; set; }
+
+    [HttpResult]
+    public IActionResult HttpResponse { get; set; }
+}
